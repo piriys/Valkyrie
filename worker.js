0f8680ad1f0324805358a7085a29e515bdacfe7d
Piriya Saengsuwarn <dev.piriys@gmail.com>
2020-01-09 23:26:15 -0600

    Files Renamed

--- worker.js
+++ worker.js
@@ -1,6 +1,5 @@
 // Discord.js-commando
 const { CommandoClient } = require('discord.js-commando');
-const Express = require('express');
 const path = require('path');
 
 // Setting up bot
@@ -29,31 +28,6 @@ discordClient.once('ready', () => {
     game: { name: 'Visual Studio Code' },
     status: 'online'
   });
-
-  // Setting up express
-  const expressApp = Express();
-  const port = process.env.PORT || 80;
-
-  expressApp.set('view engine', 'pug');
-  expressApp.set('views', './views');
-  expressApp.get('/', (request, response) => {
-    response.render('index', {
-      title: 'Valkyrie',
-      message: 'Index'
-    });
-  });
-
-  const server = expressApp.listen(port, () =>
-    console.log(`expressApp listening on port ${port}!`)
-  );
-
-  // // Terminate express/discord.js
-  // process.on('uncaughtException', () => {
-  //   server.close();
-  // });
-  // process.on('SIGTERM', () => {
-  //   server.close();
-  // });
 });
 
 discordClient.on('error', console.error);
